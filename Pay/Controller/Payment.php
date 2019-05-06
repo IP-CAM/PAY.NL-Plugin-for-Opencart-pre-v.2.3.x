49e94570d38090d62e10b02c7d3cf3a1eb21a47f
andy <andy@pay.nl>
2019-05-06 21:41:39 +0200

    Calculation correct currency amount

--- Pay/Controller/Payment.php
+++ Pay/Controller/Payment.php
@@ -130,10 +130,15 @@ class Pay_Controller_Payment extends Controller
 
             //Producten toevoegen
             foreach ($this->cart->getProducts() as $product) {
-                $priceWithTax = $this->tax->calculate($product['price'],
+                $price = $product['price'];
+                if (isset($order_info['currency_value']) && $order_info['currency_value'] != 1) {
+                    $price = $price * $order_info['currency_value'];
+                }
+
+                $priceWithTax = $this->tax->calculate($price,
                     $product['tax_class_id'], $this->config->get('config_tax'));
 
-                $tax = $priceWithTax - $product['price'];
+                $tax = $priceWithTax - $price;
 
                 $price = round($priceWithTax * 100);
                 $totalAmount += $price * $product['quantity'];
@@ -167,6 +172,12 @@ class Pay_Controller_Payment extends Controller
                         }
 
                         $totalIncl = $total_row['value'] + $total_row_tax;
+
+                        if (isset($order_info['currency_value']) && $order_info['currency_value'] != 1) {
+                            $totalIncl = $totalIncl * $order_info['currency_value'];
+                            $total_row_tax = $total_row_tax * $order_info['currency_value'];
+                        }
+
                         $apiStart->addProduct($total_row['code'], $total_row['title'], round($totalIncl * 100), 1, Pay_Helper::calculateTaxClass($totalIncl, $total_row_tax));
                     }
                 }
@@ -200,6 +211,12 @@ class Pay_Controller_Payment extends Controller
                         }
 
                         $totalIncl = $total_row['value'] + $total_row_tax;
+
+                        if (isset($order_info['currency_value']) && $order_info['currency_value'] != 1) {
+                            $totalIncl = $totalIncl * $order_info['currency_value'];
+                            $total_row_tax = $total_row_tax * $order_info['currency_value'];
+                        }
+
                         $apiStart->addProduct($total_row['code'], $total_row['title'], round($totalIncl * 100), 1, Pay_Helper::calculateTaxClass($totalIncl, $total_row_tax));
                     }
                 }
